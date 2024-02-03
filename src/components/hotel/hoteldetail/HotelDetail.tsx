2da8127ab9c0fdc396609980b92473966f990407
agustrio1 <susantoagustrio16@gmail.com>
2024-02-03 09:38:31 +0700

    add style hotel detail

--- src/components/hotel/hoteldetail/HotelDetail.tsx
+++ src/components/hotel/hoteldetail/HotelDetail.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import {hotels} from '../../../data/hotelData';
-import './HotelDetail.css'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { hotels } from "../../../data/hotelData";
+import "./HotelDetail.css";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import { useHotelDetailsStore } from "../../../store/store";
+import formatCurrency from "../../../utils/formatCurrency";
 
 const HotelDetail: React.FC = () => {
   const { id } = useParams<{ id?: string }>();
-  const hotel = id ? hotels.find((hotel) => hotel.id === parseInt(id)) : undefined;
+  const hotel = id
+    ? hotels.find((hotel) => hotel.id === parseInt(id))
+    : undefined;
+
+  const { showDescription, toggleDescription } = useHotelDetailsStore();
 
   if (!hotel) {
     return <div>Hotel not found!</div>;
   }
 
   return (
-    <div>
+    <div className="hotel-detail">
       <h2>{hotel.name}</h2>
       <img src={hotel.image} alt={hotel.name} />
       <p>Rating : {hotel.rating}</p>
-      <p>Fasilitas: {hotel.facilities.join(', ')}</p>
+      <p>Fasilitas: {hotel.facilities.join(", ")}</p>
       <p>Lokasi: {hotel.location}</p>
-      <p>Harga: {hotel.price}</p>
-      <p>Deskripsi: {hotel.description}</p>
+      <p>Harga: {formatCurrency(hotel.price)}</p>
+      <div className="description-toggle" onClick={toggleDescription}>
+        <p>Deskripsi {showDescription ? <FaAngleUp /> : <FaAngleDown />}</p>
+      </div>
+      {showDescription && <p>{hotel.description}</p>}
     </div>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -6,8 +6,19 @@ interface SidebarStore {
   closeMenu: () => void;
 }
 
+type HotelDetailsStore = {
+  showDescription: boolean;
+  toggleDescription: () => void;
+}
+
 export const useSidebarStore = create<SidebarStore>((set) => ({
   isOpen: false,
   toggleMenu: () => set((state) => ({ isOpen: !state.isOpen })),
   closeMenu: () => set({ isOpen: false }),
 }));
+
+
+export const useHotelDetailsStore = create<HotelDetailsStore>((set) => ({
+  showDescription: false,
+  toggleDescription: () => set((state) => ({ showDescription: !state.showDescription })),
+}))
